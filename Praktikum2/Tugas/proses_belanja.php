f9d67dedb1171a493905e600151dae9897b48bbc
tamassaki <tama22069si@student.nurulfikri.ac.id>
2023-03-05 15:51:19 +0700

    Upload Tugas 2

--- Praktikum2/Tugas/proses_belanja.php
+++ Praktikum2/Tugas/proses_belanja.php
@@ -6,9 +6,9 @@ $total_harga = ($produk * $jumlah);
 
 if ($produk == 'TV'){
     $total_harga = 4200000 * $jumlah;
-} elseif ($produk == 'kulkas'){
-    $total_harga = 3100000 * $jumlah;
-} elseif ($produk == 'mesin cuci'){
+} elseif ($produk == 'Kulkas'){
+    $total_harga = 3200000 * $jumlah;
+} elseif ($produk == 'Mesin Cuci'){
     $total_harga = 3800000 * $jumlah;
 } else {
     $total_harga = '';
