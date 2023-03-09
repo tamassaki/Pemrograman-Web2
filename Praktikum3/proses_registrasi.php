c6912cdff6d2abfa637b81ad757bd30cd211d4c7
tamassaki <tama22069si@student.nurulfikri.ac.id>
2023-03-09 15:32:03 +0700

    Upload Praktikum 3

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,21 @@
+<?php 
+
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Bekasi"];
+
+$program_studi =  [
+    "SI"=>"Sistem Informasi",
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital"
+];
+
+$skills = [
+    "HTML" =>10,
+    "CSS" =>10,
+    "Javascrip" =>20,
+    "RWD Boostrap" =>20,
+    "PHP" => 30,
+    "Python" => 30,
+    "Java" => 50
+]
+
+?>
